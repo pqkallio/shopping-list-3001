6d7a16b83fd4f4a0f5dd9a0d8650ec1a8bd9b0d7
Petri Kallio <kallionpetri@gmail.com>
2015-08-03 06:39:42 +0300

    disable email-confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ActiveRecord::Base
   validates :firstname, presence: true
   validates :lastname, presence: true
   validates :username, presence: true, uniqueness: true
-  validates :email, email: true
+  # Ei toiminut Herokussa:
+  # validates :email, email: true
   validate :password_validation
 
   def password_validation
